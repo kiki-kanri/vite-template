a35288c795801b04d29a04270d00cd5a0413b8b2
kiki-kanri <a470666@gmail.com>
2022-09-02 14:23:07 +0800

    修改縮排

--- src/router/index.js
+++ src/router/index.js
@@ -3,16 +3,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-    {
-        path: '/',
-        name: 'home',
-        component: () => import('@/views/Home.vue')
-    }
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('@/views/Home.vue')
+  }
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHistory(),
+  routes
 });
 
 export default router;
